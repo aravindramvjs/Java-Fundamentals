de59ff6f8b0fac6ff5d13c49ecd591c40e7a5c4d
ARAVIND RAM <116251979+aravindramvjs@users.noreply.github.com>
2023-06-21 10:42:22 +0530

    Add files via upload

---
+++ src/day14/practice/CurrentDateTime.java
@@ -0,0 +1,16 @@
+ package day14.practice;
+ 
+import java.time.LocalDateTime;
+
+public class CurrentDateTime {
+	
+	public static void main(String[] args) {
+		//Write a Java program to display the current date and time using the LocalDateTime class.
+		
+		
+		LocalDateTime timenow = LocalDateTime.now();
+		System.out.println(timenow);
+		
+	}
+	
+}
