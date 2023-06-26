e228ced34fcc54bf443a3e8999ee3bbf302f4490
aravindramvjs <aravindramvj@gmail.com>
2023-06-26 17:45:18 +0530

    Day 18 solved

---
+++ src/day18/solved/AgeValidator.java
@@ -0,0 +1,19 @@
+package day18.solved;
+
+//age validator class
+
+class AgeValidator{
+	
+//	validating age if age is greater than 0
+//	if lesser than zero throw IllegalArgumentException
+	
+	static boolean validateage(int age) {
+		if(age <= 0) {
+			throw new IllegalArgumentException("Invalid Age");
+		}
+		else {
+			return true;
+		}
+	}
+	
+}
